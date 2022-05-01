fa405ebaa2e11cc71c6fe09d4049e5ec5712c7a6
madhoora-mohan-s <madhooramohan.s2003@gmail.com>
2022-05-01 16:00:26 +0530

    Created the project file

---
+++ actions/actions.py
@@ -0,0 +1,66 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+#
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+import csv
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+class ActionStoreCSV(Action):
+
+     def name(self) -> Text:
+         return "action_store_csv"
+     
+
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        asname=tracker.get_slot("asname")
+        cnumber=tracker.get_slot("bnumber")
+        dboard=tracker.get_slot("cboard")
+        ecourse=tracker.get_slot("dcutoff")
+        flocation=tracker.get_slot("elocation")
+        
+        with open('info.csv', 'a') as info:
+            fieldnames = ['asname', 'bnumber', 'cboard', 'dcutoff', 'elocation']
+            
+            csv_writer = csv.writer(info, delimiter='\t')
+            
+            row=[asname,bnumber,cboard,dcutoff,elocation]
+            csv_writer.writerow(row)
+        return []
+
+class ActionHelloWorld(FormAction):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
